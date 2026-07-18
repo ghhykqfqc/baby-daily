f3e7210375cab3f39a216d49ac0d7794285c5cc9
huangyk <huangyk@chinaums.com>
2026-07-18 23:55:56 +0800

    修改api\users.ts的supabase为@/引入

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '../../../supabaseClient';
+import { supabase } from '@/supabaseClient';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
